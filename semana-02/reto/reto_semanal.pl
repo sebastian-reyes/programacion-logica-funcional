4c77a635336b3444edb643647b4f34dbf3264c61
sebastian-reyes <sebastianreyes2105@gmail.com>
2024-08-23 21:22:51 -0500

    semana 02, agregando reto de calificaciones

--- semana-02/reto/reto_semanal.pl
+++ semana-02/reto/reto_semanal.pl
@@ -1,12 +1,16 @@
-% Define una base de datos con hechos sobre estudiantes y sus calificaciones. Luego,
-% escribe reglas para realizar las siguientes consultas:
-
 calificacion(john, 85).
 calificacion(mary, 92).
 calificacion(alice, 78).
 calificacion(bob, 65).
 calificacion(charlie, 88).
 
+% Reto 1: buscar la calificación según es estudiante
+calificacionDe(Estudiante, Nota) :-
+    findall(Nota,
+            calificacion(Estudiante, Nota),
+            Nota).
+    
+% Reto 2: Buscar los estudiantes con calificacion mayor a la ingresada
 estudiantesConCalificacionMayorA(Nota, Estudiantes) :-
     findall(Nombre, 
         (calificacion(Nombre, Calificacion), Calificacion > Nota),
